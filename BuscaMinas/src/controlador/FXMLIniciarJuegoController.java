9ddcd9c6e92aaec1afd4de0ad69691afc4da75af
Jonathan <fywer777@gmail.com>
2017-10-25 09:07:17 -0500

    Modificación del tablero

--- BuscaMinas/src/controlador/FXMLIniciarJuegoController.java
+++ BuscaMinas/src/controlador/FXMLIniciarJuegoController.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
@@ -27,28 +28,18 @@ public class FXMLIniciarJuegoController implements Initializable, FXMLControlabl
     @FXML
     private GridPane gpeTablero;
     @FXML
-    private ImageView ivwFotoJugador2;
-    @FXML
-    private Label lblJugador1;
-    @FXML
-    private Label lblJugador2;
-    @FXML
-    private ImageView ivwFotoJugador1;
+    private Button btnAbandonarPartida;
+
     
     private BuscaMinasApp mainApp;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        lvwMenu.setOnMouseClicked(value -> {
-            String opcion = lvwMenu.getSelectionModel().getSelectedItem();
-            switch (opcion) {
-                case "Abandonar Partida":
-                    boolean confirmacion = Dialogo.confirmacion("¿Está seguro que desea abandonar la partida?");
-                    if (confirmacion) {
-                        mainApp.intercambiarEscena(BuscaMinasApp.ESCENA_MULTIJUGADOR);
-                    }
-                    break;
-            }
+        btnAbandonarPartida.setOnAction(value -> {
+            boolean confirmacion = Dialogo.confirmacion("¿Está seguro que desea abandonar la partida?");
+                if (confirmacion) {
+                    mainApp.intercambiarEscena(BuscaMinasApp.ESCENA_MULTIJUGADOR);
+                }
         });
     }    
 
