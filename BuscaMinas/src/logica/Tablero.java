09756ad9a006d45e1fe890ab815631ca0a78c870
Erick <erick_ri@hotmail.com>
2017-10-25 11:17:24 -0500

    Tablero Metodo sortear 0.1

--- BuscaMinas/src/logica/Tablero.java
+++ BuscaMinas/src/logica/Tablero.java
@@ -10,5 +10,22 @@ package logica;
  * @author EricK
  */
 public class Tablero {
+  private int[][] arrCasilla; //Arreglo de casillas con numero de minas (0 sin minas alrededor, 1 una mina alrededor, 9 mina, etc)
+  private int numMinas; //numero de minas en el campo
+  private int tamano; //tamaño del cuadrante del tablero
+  
+  public Tablero(int numMinas, int tamano) {
+    this.numMinas = numMinas;
+    sortear();
+  }
+
+  private void sortear() {
+    for (int i = 0; i < tamano; i++) {
+      int[] is = arrCasilla[i];
+      
+    }
+  }
+  
+  
   
 }
