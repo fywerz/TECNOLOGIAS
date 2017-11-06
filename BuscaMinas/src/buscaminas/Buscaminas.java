6d8e15944770793992c2251e83edac9a5b1216c3
Erick <erick_ri@hotmail.com>
2017-11-05 20:58:37 -0600

    Pantallas

---
+++ BuscaMinas/src/buscaminas/Buscaminas.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+/**
+ *
+ * @author EricK
+ */
+public class Buscaminas extends Application {
+  
+  @Override
+  public void start(Stage primaryStage) throws IOException {
+  Parent root=FXMLLoader.load(getClass().getResource("/pantallas/FXMLInicioSesion.fxml"));  
+    
+    Scene scene = new Scene(root);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+  
+}
